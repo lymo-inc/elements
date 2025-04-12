f18f81eb63a2b1f1227846700297fb2b0ecdb5af
peaske7 <peaske@pm.me>
2025-04-13 00:46:37 +0900

    expose onChange function for file selector

--- packages/mux-uploader-react/src/mux-uploader-file-select.tsx
+++ packages/mux-uploader-react/src/mux-uploader-file-select.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import '@mux/mux-uploader';
 import type { MuxUploaderFileSelectElement } from '@mux/mux-uploader';
 import { toNativeProps } from './common/utils';
@@ -10,11 +10,32 @@ export type MuxUploaderFileSelectRefAttributes = MuxUploaderFileSelectElement;
 export type MuxUploaderFileSelectProps = {
   muxUploader?: string;
   children?: React.ReactNode;
+  onFileSelect?: (filename: string) => void;
 } & Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>, 'ref'>;
 
 const MuxUploaderFileSelectInternal = React.forwardRef<MuxUploaderFileSelectRefAttributes, MuxUploaderFileSelectProps>(
-  ({ children, ...props }, ref) => {
-    return React.createElement('mux-uploader-file-select', toNativeProps({ ...props, ref }), children);
+  ({ children, onFileSelect, ...props }, ref) => {
+    const handleFileSelect = useCallback(
+      (event: Event) => {
+        const target = event.target as MuxUploaderFileSelectElement;
+        const files = target.files;
+
+        if (files && files.length > 0) {
+          const file = files[0];
+          onFileSelect?.(file.name);
+        }
+      },
+      [onFileSelect]
+    );
+
+    return React.createElement(
+      'mux-uploader-file-select',
+      {
+        ...toNativeProps({ ...props, ref }),
+        onChange: handleFileSelect,
+      },
+      children
+    );
   }
 );
 
